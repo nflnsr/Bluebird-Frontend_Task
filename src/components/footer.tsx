91afbbc115a649c446dcbfe409fae34ac61eeb0f
Naufal Nasrullah <naufaln176@gmail.com>
2024-01-12 21:55:06 +0700

    feat(footer): add footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export function Footer() {
+  return (
+    <>
+      <footer className="w-full text-center relative h-20">
+        <div className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2">
+          <h1 className="text-xl font-semibold">Footer</h1>
+          <p className="font-semibold">Bluebird Frontend Test © 2024</p>
+        </div>
+      </footer>
+    </>
+  );
+}
