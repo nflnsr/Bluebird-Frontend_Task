5d82d893964f3e31b10f47bf8de35f2203cde007
Naufal Nasrullah <naufaln176@gmail.com>
2024-01-12 15:40:33 +0700

    feat(wishlist): add wishlist page (#7)
    * delete vercel.json
    
    * chore: add sharp package
    
    * add output config to standalone
    
    * fix: can't show image
    
    * fix: unoptimized img
    
    * feat: update the progress
    
    * feat(wishlist): add wishlist page

--- src/store/vehicles-slice.ts
+++ src/store/vehicles-slice.ts
@@ -1,4 +1,4 @@
-import axios,{ AxiosError } from "axios";
+import axios, { AxiosError } from "axios";
 import { axiosInstance } from "@/lib/axios-instance";
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { VehicleCategory, VehicleType, CarType } from "@/types";
@@ -7,6 +7,7 @@ type Data = {
   category: VehicleCategory[];
   type: VehicleType[];
   myBook: CarType[];
+  wishlist: CarType[];
   status: "idle" | "loading" | "succeeded" | "failed";
   error: string | undefined;
 };
@@ -15,24 +16,28 @@ const initialState: Data = {
   category: [] as VehicleCategory[],
   type: [] as VehicleType[],
   myBook: [] as CarType[],
+  wishlist: [] as CarType[],
   status: "idle",
   error: "",
 };
 
-export const getVehiclesData = createAsyncThunk("vehicles/getAllvehicles", async () => {
-  try {
-    const { data } = await axiosInstance.get("/vehicles");
-    return data;
-  } catch (err) {
-    if (axios.isAxiosError(err)) {
-      const axiosError: AxiosError = err;
-      console.error(axiosError.stack)
-      throw new Error(axiosError.stack);
+export const getVehiclesData = createAsyncThunk(
+  "vehicles/getAllvehicles",
+  async () => {
+    try {
+      const { data } = await axiosInstance.get("/vehicles");
+      return data;
+    } catch (err) {
+      if (axios.isAxiosError(err)) {
+        const axiosError: AxiosError = err;
+        console.error(axiosError.stack);
+        throw new Error(axiosError.stack);
+      }
+      console.error(err);
+      throw new Error(String(err));
     }
-    console.error(err)
-    throw new Error(String(err));
-  }
-});
+  },
+);
 
 export const vehiclesSlice = createSlice({
   name: "vehicles",
@@ -45,9 +50,13 @@ export const vehiclesSlice = createSlice({
           if (carItem.id === id) {
             carItem.isLiked = !carItem.isLiked;
             carItem.isLiked ? carItem.likes++ : carItem.likes--;
+            const index = state.wishlist.findIndex((car) => car.id === id);
+            const carExist = state.wishlist.find((car) => car.id === id);
+            if (!carExist) state.wishlist.push(carItem);
+            else state.wishlist.splice(index, 1);
           }
         });
-      })
+      });
     },
     addToMyBook: (state, action) => {
       const { id } = action.payload;
@@ -57,9 +66,8 @@ export const vehiclesSlice = createSlice({
             state.myBook.push(carItem);
           }
         });
-      })
-      console.log(state.myBook, 'myBook')
-    }
+      });
+    },
   },
   extraReducers: (builder) => {
     builder.addCase(getVehiclesData.pending, (state) => {
@@ -67,14 +75,18 @@ export const vehiclesSlice = createSlice({
     });
     builder.addCase(getVehiclesData.fulfilled, (state, action) => {
       let id = 1;
-      const modifiedData = action.payload.type?.map((typeItem: { car_type: CarType }) => {
-        typeItem.car_type.map((carItem: { id: string; likes: number; isLiked: boolean}) => {
-          carItem.id = String(id++);
-          carItem.likes = Math.floor(Math.random() * 10) + 1;
-          carItem.isLiked = false;
-        });
-        return typeItem;
-      });
+      const modifiedData = action.payload.type?.map(
+        (typeItem: { car_type: CarType }) => {
+          typeItem.car_type.map(
+            (carItem: { id: string; likes: number; isLiked: boolean }) => {
+              carItem.id = String(id++);
+              carItem.likes = Math.floor(Math.random() * 10) + 1;
+              carItem.isLiked = false;
+            },
+          );
+          return typeItem;
+        },
+      );
       state.status = "succeeded";
       state.category = action.payload.category;
       state.type = modifiedData;
@@ -86,11 +98,18 @@ export const vehiclesSlice = createSlice({
   },
 });
 
-export const selectVehiclesCategory = (state: { vehicles: Data }) => state.vehicles.category;
-export const selectVehiclesType = (state: { vehicles: Data }) => state.vehicles.type;
-export const selectMyBook = (state: { vehicles: Data }) => state.vehicles.myBook;
-export const vehiclesStatus = (state: { vehicles: Data }) => state.vehicles.status;
-export const vehiclesError = (state: { vehicles: Data }) => state.vehicles.error;
+export const selectVehiclesCategory = (state: { vehicles: Data }) =>
+  state.vehicles.category;
+export const selectVehiclesType = (state: { vehicles: Data }) =>
+  state.vehicles.type;
+export const selectMyBook = (state: { vehicles: Data }) =>
+  state.vehicles.myBook;
+export const selectWishlist = (state: { vehicles: Data }) =>
+  state.vehicles.wishlist;
+export const vehiclesStatus = (state: { vehicles: Data }) =>
+  state.vehicles.status;
+export const vehiclesError = (state: { vehicles: Data }) =>
+  state.vehicles.error;
 
 export const { toggleLike, addToMyBook } = vehiclesSlice.actions;
 
