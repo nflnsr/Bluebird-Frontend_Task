023631e92bbfee73171558ecde8959d68483ea26
Naufal Nasrullah <naufaln176@gmail.com>
2024-01-13 13:28:41 +0700

    fix(search-bar): fix search bar functionality

--- src/app/_components/vehicle-list.tsx
+++ src/app/_components/vehicle-list.tsx
@@ -34,13 +34,15 @@ export function VehicleList() {
             .includes(String(searchValue).toLowerCase()),
       );
     });
-    console.log(newVehicleList, "newVehicleList")
-    setNewVehicles(vehicles.map((vehicles, index) => {
-      return {
-        ...vehicles,
-        car_type: newVehicleList[index]
-      }
-    }))
+    console.log(newVehicleList, "newVehicleList");
+    setNewVehicles(
+      vehicles.map((vehicles, index) => {
+        return {
+          ...vehicles,
+          car_type: newVehicleList[index],
+        };
+      }),
+    );
   }, [searchValue, vehicles]);
 
   return (
@@ -49,65 +51,75 @@ export function VehicleList() {
         Vehicle List
       </h1>
       <section className="flex flex-wrap justify-center pb-10">
-        {(newVehicles.length !== 0 ? newVehicles : vehicles).map((vehicleType: Vehicles) => (
-          <div
-            key={vehicleType?.id}
-            className="w-full p-2 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5"
-          >
-            {vehicleType?.car_type?.map((carType: Car) => (
+        {newVehicles.every((vehicle) => vehicle?.car_type?.length === 0) && (
+          <p className="w-full text-center">Car not found!</p>
+        )}
+        {(newVehicles.length !== 0 ? newVehicles : vehicles).map(
+          (vehicleType: Vehicles) => {
+            // if(vehicleType?.car_type?.length === 0) return <>Car not found!</>
+            return (
               <div
-                key={carType?.id}
-                className="overflow-hidden rounded-lg bg-white shadow dark:bg-gray-800"
+                key={vehicleType?.id}
+                className="w-full p-2 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5"
               >
-                <Link href={`vehicle/${carType?.id}`}>
-                  <Image
-                    className="h-48 w-full object-cover object-center"
-                    src={carType?.imageURL?.replace(" ", "")}
-                    alt={carType?.vehicle}
-                    width={720}
-                    height={400}
-                    sizes="100vw"
-                    unoptimized
-                  />
-                </Link>
-                <div className="p-4">
-                  <Link
-                    href={`vehicle/${carType?.id}`}
-                    className="text-lg font-medium text-gray-800 dark:text-white"
-                  >
-                    {carType?.vehicle}
-                  </Link>
-                </div>
-
-                <div className="flex justify-between p-4">
-                  <Link
-                    href={`vehicle/${carType?.id}`}
-                    className="text-lg font-medium text-gray-800 dark:text-white"
-                  >
-                    {carType?.price}
-                  </Link>
-
-                  <div className="flex select-none items-center">
-                    <span
-                      onClick={() => {
-                        dispatch(toggleLike({ id: carType?.id }));
-                      }}
+                {vehicleType?.car_type?.map((carType: Car) => {
+                  return (
+                    <div
+                      key={carType?.id}
+                      className="overflow-hidden rounded-lg bg-white shadow dark:bg-gray-800"
                     >
-                      <Love
-                        className={`h-6 w-6 cursor-pointer ${
-                          carType?.isLiked ? "fill-red-400" : "fill-none"
-                        }`}
-                      />
-                    </span>
-                    <span className="leading-none text-gray-700 dark:text-gray-300">
-                      {carType?.likes}
-                    </span>
-                  </div>
-                </div>
+                      <Link href={`vehicle/${carType?.id}`}>
+                        <Image
+                          className="h-48 w-full object-cover object-center"
+                          src={carType?.imageURL?.replace(" ", "")}
+                          alt={carType?.vehicle}
+                          width={720}
+                          height={400}
+                          sizes="100vw"
+                          unoptimized
+                        />
+                      </Link>
+                      <div className="p-4">
+                        <Link
+                          href={`vehicle/${carType?.id}`}
+                          className="text-lg font-medium text-gray-800 dark:text-white"
+                        >
+                          {carType?.vehicle}
+                        </Link>
+                      </div>
+
+                      <div className="flex justify-between p-4">
+                        <Link
+                          href={`vehicle/${carType?.id}`}
+                          className="text-lg font-medium text-gray-800 dark:text-white"
+                        >
+                          {carType?.price}
+                        </Link>
+
+                        <div className="flex select-none items-center">
+                          <span
+                            onClick={() => {
+                              dispatch(toggleLike({ id: carType?.id }));
+                            }}
+                          >
+                            <Love
+                              className={`h-6 w-6 cursor-pointer ${
+                                carType?.isLiked ? "fill-red-400" : "fill-none"
+                              }`}
+                            />
+                          </span>
+                          <span className="leading-none text-gray-700 dark:text-gray-300">
+                            {carType?.likes}
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
               </div>
-            ))}
-          </div>
-        ))}
+            );
+          },
+        )}
       </section>
     </>
   );
